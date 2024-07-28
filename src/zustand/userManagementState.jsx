7f43ccfc6f497237f4e74ce84813f8200cc3b6c1
Brbrynt <166639038+Brbrynt@users.noreply.github.com>
2024-07-28 13:49:22 +0800

    Implemented localstorage from zustand.

--- src/zustand/userManagementState.jsx
+++ src/zustand/userManagementState.jsx
@@ -1,7 +1,20 @@
 import create from 'zustand';
 
+const localStorageKey = 'userManagementState';
+
+const getUserFromLocalStorage = () => {
+  const user = localStorage.getItem(localStorageKey);
+  return user ? JSON.parse(user) : null;
+};
+
 export const userManagementState = create((set) => ({
-  user: null, 
-  setUser: (userData) => set({ user: userData }), 
-  clearUser: () => set({ user: null }) 
+  user: getUserFromLocalStorage(),
+  setUser: (userData) => {
+    localStorage.setItem(localStorageKey, JSON.stringify(userData));
+    set({ user: userData });
+  },
+  clearUser: () => {
+    localStorage.removeItem(localStorageKey);
+    set({ user: null });
+  }
 }));
