ee6f7a9ca15bee40e2b849ebfca8f7331117a2e7
Brbrynt <166639038+Brbrynt@users.noreply.github.com>
2024-07-29 20:42:45 +0800

    Implemented userExist condition

--- src/model/deviceModel.jsx
+++ src/model/deviceModel.jsx
@@ -2,7 +2,7 @@ import { useStore } from "../zustand/userManagementState";
 
 export const DeviceModel = {
     device_name: '',
-    added_by: useStore.getState().user.userId,
+    added_by: useStore.getState().user.userId ? useStore.getState().user.userId : '',
     quantity: '',
     power: ''
 }
